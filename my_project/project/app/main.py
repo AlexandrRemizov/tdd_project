77b66f87cadea6b963e8b83946e6e885e62503a3
Alexandr Remizov <ALVRem@mail.ru>
2023-04-20 19:08:46 +0300

    add alembic for migrations

--- my_project/project/app/main.py
+++ my_project/project/app/main.py
@@ -14,10 +14,10 @@ app = FastAPI()
 #async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
-@app.on_event("startup")
-async def create_table():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+#@app.on_event("startup")
+#async def create_table():
+#    async with engine.begin() as conn:
+#        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.get("/ping")
